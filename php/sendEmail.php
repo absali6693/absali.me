2de75aa98f67f0846be69805ac6bf4b6bf21e870
Abbas Ali <absali@terpmail.umd.edu>
2018-12-29 20:05:01 -0500

    Updates

--- php/sendEmail.php
+++ php/sendEmail.php
@@ -1,4 +1,7 @@
 <?php
+    //Access-Control-Allow-Origin header with wildcard.
+    header('Access-Control-Allow-Origin: *');
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $sender = filter_var(trim($_POST["form_email"]), FILTER_SANITIZE_EMAIL); // trim() strips any white space from beginning and end of the string
         $message = strip_tags($_POST["form_msg"]); // strip of all HTML and PHP tags
