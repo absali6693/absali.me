d4d76d19462a9c67ba84a3fd1304ba4ecd5f5e3e
Abbas  Ali <absali@wireless-10-105-76-200.umd.edu>
2019-03-08 15:02:25 -0500

    updates structure

---
+++ build/precache-manifest.2c622741145dec886676de83a684d3e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "62a8d3a9890d0a0518cc",
+    "url": "/static/js/main.39468b59.chunk.js"
+  },
+  {
+    "revision": "83df5ea79fd18db37c7d",
+    "url": "/static/js/2.69c9f46b.chunk.js"
+  },
+  {
+    "revision": "62a8d3a9890d0a0518cc",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "312652bf2f0a6c4cd03f0c27b41f3134",
+    "url": "/index.html"
+  }
+];
